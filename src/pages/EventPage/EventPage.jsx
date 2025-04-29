e9f941c1a40758e7793434b32e8f35a614ec15d4
isabellarssonn <isabel.larsson@hotmail.com>
2025-04-29 09:37:43 +0200

    fixat header-komponent

--- src/pages/EventPage/EventPage.jsx
+++ src/pages/EventPage/EventPage.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Header from '../../components/Header/Header'
 
 function EventPage() {
   return (
-    <div>EventPage</div>
+    <div>
+      <Header title="Events" />
+    </div>
   )
 }
 
