abebdb1f80070e6fcc524f57f218681be6010ada
isabellarssonn <isabel.larsson@hotmail.com>
2025-05-05 14:50:33 +0200

    send order-funktion klar

---
+++ src/components/TicketCard/TicketCard.jsx
@@ -0,0 +1,36 @@
+import "./ticketCard.css"
+
+function TicketCard({ event }) {
+  return (
+    <article className='ticketcard'>
+        <div className='what-section'>
+            <p className='ticket__info'>What</p>
+            <h2 className='ticketcard__name'>{event.name}</h2>
+        </div>
+        <div className='where-section'>
+            <p className='ticket__info'>Where</p>
+            <h3 className='ticket__where'>{event.where}</h3>
+        </div>
+        <div className='date-section'>
+            <div className='when-section'>
+                <p className='ticket__info'>When</p>
+                <p className='ticket__when'>{event.when.date}</p>
+            </div>
+            <div className='from-section'>
+                <p className='ticket__info'>1234424</p>
+                <p className='ticket__from'>{event.when.from}</p>
+            </div>
+            <div className='to-section'>
+                <p className='ticket__info'>To</p>
+                <p className='ticket__to'>{event.when.to}</p>
+            </div>
+        </div>
+        <div className='info-section'>
+            <p className='ticket__info'>Info</p>
+            <p className='ticket__seat'>Section D - seat 326</p>
+        </div>
+    </article>
+  )
+}
+
+export default TicketCard
--- src/pages/TicketPage/TicketPage.jsx
+++ src/pages/TicketPage/TicketPage.jsx
@@ -1,9 +1,11 @@
 import "./ticketPage.css"
+import TicketList from "../../components/TicketList/TicketList";
 
 function TicketPage() {
+
   return (
     <section className='ticket-page--special'>
-        Ticketpage
+      <TicketList />
     </section>
   )
 }
